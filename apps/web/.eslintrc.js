ec6cdfced67578fbf6f27bff71a37046757284e2
galvinchau <payroll.blueangelscare@gmail.com>
2025-09-07 20:20:55 -0400

    chore(eslint): web extends next/core-web-vitals + @bac/eslint-config directly

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,5 +1,7 @@
-/** ESLint config for Next.js app */
+/** ESLint config for apps/web (Next.js) */
 module.exports = {
   root: true,
-  extends: ["next/core-web-vitals", "prettier"]
+  extends: ['next/core-web-vitals', '@bac/eslint-config'],
+  parserOptions: { ecmaVersion: 2021, sourceType: 'module' },
+  ignorePatterns: ['.next', 'node_modules', 'dist', 'build', 'coverage'],
 };
