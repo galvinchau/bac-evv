4c594479ca4d31553233d9231d3310581714329f
galvinchau <payroll.blueangelscare@gmail.com>
2025-09-09 15:01:14 -0400

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT ? Number(process.env.PORT) : 3001;
-  await app.listen(port);
-  console.log(`API listening on http://localhost:${port}`);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
