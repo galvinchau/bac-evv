cdd7e9ea97b2633de555a9b4ecd055791ceacafb
galvinchau <payroll.blueangelscare@gmail.com>
2025-09-12 10:10:10 -0400

    feat(api): register NotesController in AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,16 @@
-﻿import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HealthController } from './health.controller';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { CommonModule } from "./common/common.module";
+import { HealthController } from "./health.controller";
+import { S3Controller } from "./s3.controller";
+import { NotesController } from "./notes/notes.controller";
 
 @Module({
-  imports: [],
-  controllers: [AppController, HealthController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CommonModule,
+  ],
+  controllers: [HealthController, S3Controller, NotesController],
+  providers: [],
 })
 export class AppModule {}
