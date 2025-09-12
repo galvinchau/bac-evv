2fdd75e6b1f60b68c3581ab2e5a3c110dbf0d944
galvinchau <payroll.blueangelscare@gmail.com>
2025-09-12 10:48:23 -0400

    Update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,16 +1,9 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { CommonModule } from "./common/common.module";
 import { HealthController } from "./health.controller";
-import { S3Controller } from "./s3.controller";
 import { NotesController } from "./notes/notes.controller";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    CommonModule,
-  ],
-  controllers: [HealthController, S3Controller, NotesController],
+  controllers: [HealthController, NotesController],
   providers: [],
 })
 export class AppModule {}
